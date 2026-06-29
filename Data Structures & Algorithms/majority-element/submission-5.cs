df4f519592ef53c5c5a45f40bc88c51a4c660c44
Abdullahi Abubakar Siddiq <122829204+Simply-Incognito@users.noreply.github.com>
2026-06-29 19:36:06 +0100

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        Array.Sort(nums);
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach (int num in nums) {
+            //!dict.ContainsKey(num) ? dict.Add(num, 1) : dict[num]++;
+           if (!dict.ContainsKey(num)) {
+            dict.Add(num, 1);
+           } else {
+            dict[num]++;
+           }
+        }
+    int maxCount = 0;
+        foreach (var kvp in dict) {
+            if (kvp.Value > (nums.Length / 2)) {
+                maxCount = kvp.Key;
+                break;
+            }
+        }
+        return maxCount;
+        
+    }
+}
